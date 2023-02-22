df835a29893ae766fed4ca33ecaa2197a9a6a78c
Quoc Bao <daoquocbao28@gmail.com>
2023-02-22 22:43:57 +0700

    gen log on CI

--- teste2e.sh
+++
@@ -1,6 +0,0 @@
-rm /integration_test/output-temp.txt
-flutter drive --driver test_driver/integration_test_driver.dart --target integration_test/signin_test.dart -d chrome |./integration_test/testout.sh
-flutter drive --driver test_driver/integration_test_driver.dart --target integration_test/signup_test.dart -d chrome |./integration_test/testout.sh
-flutter drive --driver test_driver/integration_test_driver.dart --target integration_test/forgot_password_test.dart -d chrome |./integration_test/testout.sh
-flutter drive --driver test_driver/integration_test_driver.dart --target integration_test/personal_budget_test.dart -d chrome |./integration_test/testout.sh
-flutter drive --driver test_driver/integration_test_driver.dart --target integration_test/profile_test.dart -d chrome |./integration_test/testout.sh
