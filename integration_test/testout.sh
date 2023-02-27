5468cd500e2e2e568e3320c9a65275254fcaac2c
baovlorishqa <122070099+baovlorishqa@users.noreply.github.com>
2023-02-27 17:57:46 +0700

    Update testout.sh

--- integration_test/testout.sh
+++ integration_test/testout.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
-cat 1>> ./integration_test/output-temp.txt
-cat ./integration_test/output-temp.txt | tr -d '\000' > ./integration_test/output.txt
-if cat ./integration_test/output.txt | grep -q '^Failure Details:'; then
-if ! cat ./integration_test/output.txt | grep -q '^The value of ErrorWidget.builder was changed by the test.'; then
+DIR="./integration_test/output.txt"
+cat $DIR
+cat 1> ./integration_test/output-temp.txt
+cat ./integration_test/output-temp.txt | tr -d '\000' > $DIR
+if cat $DIR | grep -q '^Failure Details:'; then
+if ! cat $DIR | grep -q '^The value of ErrorWidget.builder was changed by the test.'; then
 echo -e '==========================================================='
 echo -e '= ERROR DETAILS                                           ='
 echo -e '==========================================================='
-cat ./integration_test/output.txt | grep '^Error'
-cat ./integration_test/output.txt | grep -A5000 '^Failure Details:'
+cat $DIR | grep '^Error'
+cat $DIR | grep -A5000 '^Failure Details:'
 fi
 fi
 echo -e '==========================================================='
 echo -e '= STEPS                                                   ='
 echo -e '==========================================================='
-cat ./integration_test/output.txt | grep -M  '^@\-@\-.*@\|@\|$' | sed -e 's/\(@-@-,\)//g' | sed -e 's/\(,@|@|\)//g' | grep '\"STARTED\"$'
-cat ./integration_test/output.txt | grep -M  '^@\=@\=.*@\+@\+$' | sed -e 's/\(@=@=,\)//g' | sed -e 's/\(,@+@+\)//g'
-cat ./integration_test/output.txt | grep -M  '^@\-@\-.*@\|@\|$' | sed -e 's/\(@-@-,\)//g' | sed -e 's/\(,@|@|\)//g' | grep -v '\"STARTED\"$' | tail -1 | grep -v '\"Passed\"$' | sed 's/BEFORE/Failed/g'
+cat $DIR | pcregrep -M  '^@\-@\-.*@\|@\|$' | sed -e 's/\(@-@-,\)//g' | sed -e 's/\(,@|@|\)//g' | grep '\"STARTED\"$'
+cat $DIR | pcregrep -M  '^@\=@\=.*@\+@\+$' | sed -e 's/\(@=@=,\)//g' | sed -e 's/\(,@+@+\)//g'
+cat $DIR | pcregrep -M  '^@\-@\-.*@\|@\|$' | sed -e 's/\(@-@-,\)//g' | sed -e 's/\(,@|@|\)//g' | grep -v '\"STARTED\"$' | tail -1 | grep -v '\"Passed\"$' | sed 's/BEFORE/Failed/g'
 echo -e '==========================================================='
 echo -e '= ERROR SUMMARY                                           ='
 echo -e '==========================================================='
-cat ./integration_test/output.txt | sed -n -e '/^The following TestFailure object was thrown running a test:/,/^When the exception was thrown, this was the stack:$/ p' | grep -v '^When the exception was thrown, this was the stack:$'
-if cat ./integration_test/output.txt | grep -q -v '^The following TestFailure object was thrown running a test:'; then
-cat ./integration_test/output.txt | sed -n -e '/^The following TestFailure object was thrown running a test:/,/^When the exception was thrown, this was the stack:$/ p' | grep -v '^When the exception was thrown, this was the stack:$'
+cat $DIR | sed -n -e '/^The following TestFailure object was thrown running a test:/,/^When the exception was thrown, this was the stack:$/ p' | grep -v '^When the exception was thrown, this was the stack:$'
+if cat $DIR | grep -q -v '^The following TestFailure object was thrown running a test:'; then
+cat $DIR | sed -n -e '/^The following TestFailure object was thrown running a test:/,/^When the exception was thrown, this was the stack:$/ p' | grep -v '^When the exception was thrown, this was the stack:$'
 fi
-if cat ./integration_test/output.txt | grep -q -v '^The following StateError was thrown running a test:'; then
-cat ./integration_test/output.txt | sed -n -e '/^The following StateError was thrown running a test:/,/^When the exception was thrown, this was the stack:$/ p' | grep -v '^When the exception was thrown, this was the stack:$'
+if cat $DIR | grep -q -v '^The following StateError was thrown running a test:'; then
+cat $DIR | sed -n -e '/^The following StateError was thrown running a test:/,/^When the exception was thrown, this was the stack:$/ p' | grep -v '^When the exception was thrown, this was the stack:$'
 fi
